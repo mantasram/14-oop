c569d9f63b4f3b848726876c9c6bd8f18b634db5
mramo21 <mantasramo@gmail.com>
2021-05-19 20:57:14 +0300

    dog class

---
+++ index.js
@@ -0,0 +1,7 @@
+const Dog = require('./js/Dog');
+
+const rexas = new Dog('Rex', 'brown');
+const spaikas = new Dog('Spike', 'white');
+
+console.log(rexas.hasTail);
+console.log(spaikas.hasTail);
---
+++ js/Dog.js
@@ -0,0 +1,11 @@
+class Dog {
+    constructor (name, fur) {
+        this.name = name;
+        this.furColor = fur;
+        this.legsCount = 4;
+        this.hasTail = true;
+    }
+
+}
+
+module.exports = Dog;
